b438211865c3588280403948e50a869ec29e938d
eri-akiyama <eri.akiyama@gu3.co.jp>
2012-03-29 19:33:15 +0900

    ログイン認証の修正
    ログイン認証できていなかったので、
    ログインユーザだけがアクセスできるように制限をかけた。
    ※login_required デコレータは分からなかったので使わなかったです。

--- prof/views.py
+++ prof/views.py
@@ -7,10 +7,18 @@
 
 #--------- import ---------
 from django.shortcuts import render_to_response
-
+from django.http import HttpResponseRedirect
+from django.template import RequestContext
+from inu2.prof.models import Prof_data
 
 #------------------------------------------------
 # @breaf : プロフ管理ページ呼び出し
 #------------------------------------------------
 def index(request):
-    return render_to_response('prof/index.html', {"request":request})
+    # ログインしていないのに直接 http://localhost:8000/prof/をたたいた時
+    if not request.user.is_authenticated():
+        return render_to_response('accounts/index_error.html')
+
+    # ログインしている状態の時
+    latest_prof_list = Prof_data.objects.all()
+    return render_to_response('prof/index.html', {"latest_prof_list":latest_prof_list},context_instance = RequestContext(request))
